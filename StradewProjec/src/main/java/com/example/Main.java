acfcec8c9760b1aa36fcca91c26023d0432e4de9
mehrad <mehrad.hesari06@gmail.com>
2025-04-24 16:20:18 +0330

    gson

--- StradewProjec/src/main/java/com/example/Main.java
+++ StradewProjec/src/main/java/com/example/Main.java
@@ -1,8 +1,38 @@
 package com.example;
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
+//    public static class Player {
+//        private String name;
+//        private int level;
+//        private boolean online;
+//
+//        public Player(String name, int level, boolean online) {
+//            this.name = name;
+//            this.level = level;
+//            this.online = online;
+//        }
+//    }
+
     public static void main(String[] args) {
+
+
     }
+//    public class salam
+//    {
+//        static Player player = new Player("Archer007", 12, true);
+//        static Gson gson = new Gson();
+//        static {
+//            try (FileWriter writer = new FileWriter("player.json")) {
+//                gson.toJson(player, writer);
+//                System.out.println("Saved to player.json!");
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 }
