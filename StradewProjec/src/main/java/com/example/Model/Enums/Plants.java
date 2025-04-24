a49f16cef8c5e400fb70465ceb341c5dfdb10453
mehrad <mehrad.hesari06@gmail.com>
2025-04-24 19:37:39 +0330

    plants enum

--- StradewProjec/src/main/java/com/example/Model/Enums/Plants.java
+++ StradewProjec/src/main/java/com/example/Model/Enums/Plants.java
@@ -1,8 +1,51 @@
 package com.example.Model.Enums;
 
+import java.util.ArrayList;
+
 public enum Plants {
-    BlueJazz("a" , "a" , "a" , 1 , false , 1 ,1 , true , 1 , "mamad" , true);
-    public final String Name;
+    BLUE_JAZZ("Jazz Seeds", "1-2-2-2", 7, true, 0, 50, true, 45, "Spring", false),
+    CARROT("Carrot Seeds", "1-1-1", 3, true, 0, 35, true, 75, "Spring", false),
+    CAULIFLOWER("Cauliflower Seeds", "1-2-4-4-1", 12, true, 0, 175, true, 75, "Spring", true),
+    COFFEE_BEAN("Coffee Bean", "1-2-2-3-2", 10, false, 2, 15, false, 0, "Spring & Summer", false),
+    GARLIC("Garlic Seeds", "1-2-2-2", 4, true, 0, 60, true, 20, "Spring", false),
+    GREEN_BEAN("Bean Starter", "1-1-1-3-4", 10, false, 3, 40, true, 25, "Spring", false),
+    KALE("Kale Seeds", "1-2-2-1", 6, true, 0, 110, true, 50, "Spring", false),
+    PARSNIP("Parsnip Seeds", "1-1-2", 4, true, 0, 35, true, 25, "Spring", false),
+    POTATO("Potato Seeds", "1-1-1-2-1", 6, true, 0, 80, true, 25, "Spring", false),
+    RHUBARB("Rhubarb Seeds", "2-2-2-3-4", 13, true, 0, 220, false, 0, "Spring", false),
+    STRAWBERRY("Strawberry Seeds", "1-1-2-2", 8, false, 4, 120, true, 50, "Spring", false),
+    TULIP("Tulip Bulb", "1-1-2", 6, true, 0, 30, true, 45, "Spring", false),
+    UNMILLED_RICE("Rice Shoot", "1-2-2-3", 6, true, 0, 30, true, 3, "Spring", false),
+    BLUEBERRY("Blueberry Seeds", "1-3-3-4-2", 13, false, 4, 50, true, 25, "Summer", false),
+    CORN("Corn Seeds", "2-3-3-3-3", 14, false, 4, 50, true, 25, "Summer & Fall", false),
+    HOPS("Hops Starter", "1-1-2-3-4", 11, false, 1, 25, true, 20, "Summer", false),
+    HOT_PEPPER("Pepper Seeds", "1-2-2-2-2", 8, false, 3, 40, true, 25, "Summer", false),
+    MELON("Melon Seeds", "1-2-3-3-3", 12, true, 0, 250, true, 113, "Summer", true),
+    POPPY("Poppy Seeds", "1-2-2", 7, true, 0, 140, true, 45, "Summer", false),
+    RADISH("Radish Seeds", "2-1-2-1", 6, true, 0, 90, true, 45, "Summer", false),
+    RED_CABBAGE("Red Cabbage Seeds", "1-1-2-2-2", 9, true, 0, 260, true, 75, "Summer", false),
+    STARFRUIT("Starfruit Seeds", "2-2-3-3-3", 13, true, 0, 750, true, 125, "Summer", false),
+    SUMMER_SPANGLE("Spangle Seeds", "1-2-3-1", 8, true, 0, 90, true, 45, "Summer", false),
+    SUMMER_SQUASH("Summer Squash Seeds", "1-1-1-2-1", 6, false, 3, 45, true, 63, "Summer", false),
+    SUNFLOWER("Sunflower Seeds", "1-2-3-2", 8, true, 0, 80, true, 45, "Summer & Fall", false),
+    TOMATO("Tomato Seeds", "1-2-2-1-2", 11, false, 4, 60, true, 20, "Summer", false),
+    WHEAT("Wheat Seeds", "1-1-1", 4, true, 0, 25, false, 0, "Summer & Fall", false),
+    AMARANTH("Amaranth Seeds", "1-2-2-2", 7, true, 0, 150, true, 50, "Fall", false),
+    ARTICHOKE("Artichoke Seeds", "1-2-3-4", 8, true, 0, 160, true, 30, "Fall", false),
+    BEET("Beet Seeds", "1-1-2", 6, true, 0, 100, true, 30, "Fall", false),
+    BOK_CHOY("Bok Choy Seeds", "1-1-2", 4, true, 0, 80, true, 25, "Fall", false),
+    BROCCOLI("Broccoli Seeds", "2-2-1-2", 7, true, 0, 70, true, 63, "Fall", false),
+    CRANBERRIES("Cranberry Seeds", "1-1-2-3", 10, false, 5, 75, true, 38, "Fall", false),
+    EGGPLANT("Eggplant Seeds", "1-1-1-1", 5, false, 5, 60, true, 20, "Fall", false),
+    FAIRY_ROSE("Fairy Seeds", "2-2-3-2", 12, true, 0, 290, true, 45, "Fall", false),
+    GRAPE("Grape Starter", "1-2-3-3", 10, false, 3, 80, true, 38, "Fall", false),
+    PUMPKIN("Pumpkin Seeds", "1-2-3-4-3", 13 , true , 0 , 320 , false , 0 , "Fall", true),
+    YAM("Yam Seeds" , "1-3-3-3" , 10 , true , 0 , 160 , true , 45 , "Fall", false),
+    SWEET_GEM_BERRY("Rare Seed" , "2-4-6-6-6" , 24 , true , 0 , 3000 , false , 0 , "Fall", false),
+    POWDERMELON("Powdermelon Seeds" , "1-2-1-2-1" , 7 , true , 0 , 60 , true , 63 , "Winter" , true),
+    ANCIENT_FRUIT("Ancient Seeds" , "2-7-7-7-5" , 28 , false , 7 , 550 , false , 0 ,  "Spring & Summer & Fall", false)
+    ;
+
     public final String Source;
     public final String stage;
     public final int TotalTimeHarvest;
@@ -13,8 +56,10 @@ public enum Plants {
     public final int energy;
     public final String season;
     public final boolean CanBecomeGiant;
-    Plants(String name, String source, String stage, int totalTimeHarvest, boolean onTime, int regrowthTime, int basePrice, boolean isEdible, int energy, String season, boolean canBecomeGiant) {
-        Name = name;
+
+    private static final ArrayList<Plants> loadedPlants = new ArrayList<>();
+
+    Plants(String source, String stage, int totalTimeHarvest, boolean onTime, int regrowthTime, int basePrice, boolean isEdible, int energy, String season, boolean canBecomeGiant) {
         Source = source;
         this.stage = stage;
         TotalTimeHarvest = totalTimeHarvest;
@@ -26,4 +71,6 @@ public enum Plants {
         this.season = season;
         CanBecomeGiant = canBecomeGiant;
     }
+
+
 }
